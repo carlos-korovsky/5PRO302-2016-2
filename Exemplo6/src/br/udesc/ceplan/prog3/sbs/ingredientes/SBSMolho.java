3f352c7fb67c05814ab5e1821f7b920c41b205b5
korovsky <korovsky@ukmob01.uktech.com.br>
2016-09-12 22:33:32 -0300

    Atualizada fábrica de ingredientes de pizza de SBS

---
+++ Exemplo6/src/br/udesc/ceplan/prog3/sbs/ingredientes/SBSMolho.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.udesc.ceplan.prog3.sbs.ingredientes;
+
+import br.udesc.ceplan.prog3.ingredientes.Molho;
+
+/**
+ *
+ * @author Carlos Alberto Cipriano Korovsky <carlos.korovsky@uktech.com.br>
+ */
+public class SBSMolho extends Molho {
+
+    @Override
+    public String getMolho() {
+        return "Molho de tomate orgânico de sbs";
+    }
+    
+}
