545e30921637a3e7343c760b4326a9fa7616a4fa
Carlos Alberto Cipriano Korovsky <carlos.korovsky@uktech.com.br>
2017-05-15 16:57:34 -0300

    Atualizado o nome do exemplo.

---
+++ Exemplo8/src/main/java/br/udesc/ceplan/prog3/exemplo8/Exemplo8.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.udesc.ceplan.prog3.exemplo8;
+
+
+
+/**
+ *
+ * @author Carlos Alberto Cipriano Korovsky <carlos.korovsky at gmail.com>
+ */
+public class Exemplo8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        MenuComponent menuBase = new Menu("Menu base", "Todos os menus");
+        MenuComponent menuCafeManha = new Menu("Menu Café", "Menu de café da manhã");
+        menuCafeManha.add(new MenuItem("Café", "Café preto", 1.00));
+        menuCafeManha.add(new MenuItem("Café com leite", "Café com leite", 2.00));
+        menuBase.add(menuCafeManha);
+        MenuComponent menuAlmoco = new Menu("Menu Almoço", "Menu de almoço");
+        menuAlmoco.add(new MenuItem("Bife a cavalo", "Bife a cavalo", 10.00));
+        menuAlmoco.add(new MenuItem("Bife a parmegiana", "Bife a parmegiana", 20.00));
+        menuBase.add(menuAlmoco);
+        menuBase.print();
+    }
+    
+}
---
+++ Exemplo8/src/main/java/br/udesc/ceplan/prog3/exemplo8/Menu.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.udesc.ceplan.prog3.exemplo8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Carlos Alberto Cipriano Korovsky <carlos.korovsky at gmail.com>
+ */
+public class Menu extends MenuComponent {
+    private List<MenuComponent> menuComponents;
+    private String name;
+    private String description;
+
+    public Menu(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.menuComponents = new ArrayList<>();
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void print() {
+        System.out.print("MENU:  " + this.getName());
+        System.out.println(",  " + this.getDescription());
+        System.out.println("X---------------------------------------X");
+        for (MenuComponent menuComponent: this.menuComponents) {
+            menuComponent.print();
+        }
+        System.out.println("-----------------------------------------");
+    }
+
+    @Override
+    public MenuComponent getChild(int i) {
+        return this.menuComponents.get(i);
+    }
+
+    @Override
+    public void remove(MenuComponent menuComponent) {
+        this.menuComponents.remove(menuComponent);
+    }
+
+    @Override
+    public void add(MenuComponent menuComponent) {
+        this.menuComponents.add(menuComponent);
+    }
+    
+}
